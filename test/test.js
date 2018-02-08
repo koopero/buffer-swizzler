f1fa83117e04d2be509a28aef79ae145b31adba9
Samm Cooper <samm@hfour.ca>
2018-02-07 17:56:28 -0800

    Another test

--- test/test.js
+++ test/test.js
@@ -13,7 +13,15 @@ describe('Swizzler', () => {
     let result = swizzler.swizzle( input )
 
     assert( Buffer.isBuffer( result ) )
+  })
 
+  it(`actually swizzles`, () => {
+    let swizzler = new Swizzler('AB:A')
+    let input = Buffer.from( [ 1, 2, 3, 4])
+    let result = swizzler.swizzle( input )
+    assert.equal( result.length, 2 )
+    assert.equal( result[0], 1 )
+    assert.equal( result[1], 3 )
 
   })
 })
